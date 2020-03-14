d9743081ab5e2b8737a94ae6b91eec12e4632b6c
sheff146 <alexandre.shevchenko@gmail.com>
2020-03-14 18:37:53 +0100

    Remove unnecessary scripts and deps

--- test/piglatin.test.ts
+++ test/piglatin.test.ts
@@ -1,14 +1,14 @@
-import DummyClass from "../src/piglatin"
+import DummyClass from "../src/piglatin";
 
 /**
  * Dummy test
  */
 describe("Dummy test", () => {
   it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
-  })
+    expect(true).toBeTruthy();
+  });
 
   it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
-})
+    expect(new DummyClass()).toBeInstanceOf(DummyClass);
+  });
+});
